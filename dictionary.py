34feb98d468ef3c7d3ab56971960443a18ba17ba
rez2005 <rezrezrez2005@gmail.com>
2025-10-28 10:10:49 +0800

    a

--- dictionary.py
+++ dictionary.py
@@ -11,3 +11,10 @@ favroite_numbers['a']='5'
 del favroite_numbers['b']
 print(f"{favroite_numbers}")
 print(f"{favroite_numbers.get('b','nothing')}")
+for letter,number in favroite_numbers.items():
+	print(f"{letter.title()}:{number}\n")
+rivercountry = {'nile':'Egypt','yangtze':'China','Amazon':'Brazil'}
+for river in rivercountry.keys():
+	print(river)
+for country in rivercountry.values():
+	print(country)
